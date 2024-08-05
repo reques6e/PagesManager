15e32ae9c7512de1fb0f83bb8769bf26d98bba17
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-08-05 15:12:39 +0300

    🔨 Рефакторинг кода

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-# Бета файл, его не нужно трогать
+# Бета файл, его не нужно трогать (говнокод)
 import sys
-from pathlib import Path
 import uvicorn
 
-# Добавление пути к папке 'web' в sys.path
+from pathlib import Path
+from web import run as web_module
+
 web_dir = Path(__file__).parent / 'web'
 sys.path.insert(0, str(web_dir))
 
-# Импорт web
-import web.web as web_module
 
-# Запуск сервера Uvicorn
 if __name__ == "__main__":
     uvicorn.run(
-        app='web.web:api', 
+        app='web.run:api', 
         host=web_module.Config.RUN_HOST, 
         port=web_module.Config.RUN_PORT,
         reload=web_module.Config.RUN_RELOAD
