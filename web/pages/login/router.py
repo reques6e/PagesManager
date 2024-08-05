c38db0b7ca02d9c7f60c42bcee12e28cd743edfd
reques6e <voronin99borja61275y@rambler.ru>
2024-08-05 15:00:08 +0300

    ✨ Добавил роутер для page Login

--- web/pages/login/router.py
+++ web/pages/login/router.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import JSONResponse
 
@@ -11,11 +11,37 @@ router = APIRouter(
     tags=['Pickup point']
 )
 
-
 @router.get('/')
 async def login(
-    request: Request, error: str = None
+    request: Request, 
+    error: str = None
 ): 
+    if request.cookies.get('x-fastapicore-session') == 'test':
+        return RedirectResponse(url="/dashboard", status_code=303)
+
+    error = request.session.get("error")
+    if error:
+        del request.session["error"]
+
     return templates.TemplateResponse(
         request=request, name="login.html", context={'error': error}
     )
+
+@router.post("/oauth/check")
+async def handle_login(request: Request):
+    if request.cookies.get('x-fastapicore-session') == 'test':
+        return RedirectResponse(url="/dashboard", status_code=303)
+
+    form_data = await request.form()
+    username = form_data.get('username')
+    password = form_data.get('password')
+
+    if password != "root" or username != "admin":
+        request.session["error"] = "Invalid credentials"
+        return RedirectResponse(url="/login", status_code=303)
+
+    response = HTMLResponse(content="Login successful!")
+    response.set_cookie(key='x-fastapicore-session', value='test')
+    
+    return response
+
