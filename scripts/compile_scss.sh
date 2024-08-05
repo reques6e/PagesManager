51b65e91b3f0c019690621dd46a6d19c56d879cc
reques6e <voronin99borja61275y@rambler.ru>
2024-08-05 14:57:59 +0300

    ♻️ Обновил компилятор scss для новой структуры

--- scripts/compile_scss.sh
+++ scripts/compile_scss.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # Выполнение команды sassc
-sassc web/ui/static/scss/styles.scss web/ui/static/css/styles.css
+sassc web/ui/static/scss/login.scss web/ui/static/css/login.css
 
-# Проверка кода завершения предыдущей команды
 if [ $? -eq 0 ]; then
-    # Команда выполнена успешно
     echo "SCSS успешно скомпилирован"
     exit 0
 else
-    # Команда завершилась с ошибкой
     echo "Ошибка компиляции SCSS"
     exit 1
 fi
