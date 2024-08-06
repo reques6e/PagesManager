3bc7a196a291948acd9404aaaf0f0bc4846c4484
Astamur Ladariya <149709871+reques6e@users.noreply.github.com>
2024-08-06 19:10:16 +0300

    ♻️ Автоматизировал работу компилятора

--- scripts/compile_scss.sh
+++ scripts/compile_scss.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-# Выполнение команды sassc
-sassc web/ui/static/scss/login.scss web/ui/static/css/login.css
+# Директория с файлами SCSS
+scss_dir="web/ui/static/scss"
+# Директория для сохранения файлов CSS
+css_dir="web/ui/static/css"
 
-if [ $? -eq 0 ]; then
-    echo "SCSS успешно скомпилирован"
-    exit 0
-else
-    echo "Ошибка компиляции SCSS"
-    exit 1
-fi
+# Перебираем все файлы SCSS в директории
+for scss_file in "$scss_dir"/*.scss; do
+    # Получаем имя файла без пути и расширения
+    filename=$(basename "$scss_file" .scss)
+    # Компилируем SCSS в CSS
+    sassc "$scss_file" "$css_dir/$filename.css"
+
+    # Проверяем статус последней команды
+    if [ $? -ne 0 ]; then
+        echo "Ошибка компиляции SCSS: $scss_file"
+        exit 1
+    fi
+done
+
+echo "Все файлы SCSS успешно скомпилированы"
+exit 0
