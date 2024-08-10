1b39eb0bdd93c2347935c837ca39a05661ad330a
reques6e <voronin99borja61275y@rambler.ru>
2024-08-10 15:00:24 +0300

    🔨 Обновил модели Session

--- web/cores/models/session.py
+++ web/cores/models/session.py
@@ -3,6 +3,8 @@ from pydantic import BaseModel
 class GetSession(BaseModel):
     _session: str
     user_id: int
+    login: str
+    password: str
     is_admin: int
     cookie_create_time: int # UNIX Time
     ip: str # TODO: нужно совместить с ipv6, но пока что лень
@@ -11,6 +13,8 @@ class GetSession(BaseModel):
 
 class PayloadSession(BaseModel):
     user_id: int
+    login: str
+    password: str
     is_admin: int
     cookie_create_time: int # UNIX Time
     ip: str # TODO: нужно совместить с ipv6, но пока что лень
