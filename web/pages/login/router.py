6cb562757f0d915327b128627de842fec14ea449
reques6e <voronin99borja61275y@rambler.ru>
2024-08-10 14:59:58 +0300

    ✨ Добавил Session

--- web/pages/login/router.py
+++ web/pages/login/router.py
@@ -5,6 +5,13 @@ from fastapi.responses import JSONResponse
 
 from web.utils import JSONBuildResponse
 from web.pages.base import templates
+from web.database import DataBase
+from web.cores.session import SessionManager, SessionStorage
+from web.cores.models.session import PayloadSession
+
+db = DataBase()
+storage = SessionStorage(name='session', secret='asdfdsfsdf')
+session_manager = SessionManager(storage)
 
 router = APIRouter(
     prefix='/login',
@@ -36,12 +43,30 @@ async def handle_login(request: Request):
     username = form_data.get('username')
     password = form_data.get('password')
 
-    if password != "root" or username != "admin":
+
+    lg = await db.auth_user_by_login_and_password(
+        login=username, 
+        password=password
+    )
+    print(lg) # (222, 'admin', 'root', '0', 1, 43534534, '192.168.0.0', '34534534534')
+
+    if lg is None:
         request.session["error"] = "Invalid credentials"
         return RedirectResponse(url="/login", status_code=303)
 
+    data = PayloadSession(
+        user_id=lg[0],
+        login=lg[1],
+        password=lg[2],
+        is_admin=lg[4],
+        cookie_create_time=lg[5],
+        ip=lg[6],
+        token=lg[7]
+    )
+    session = await session_manager.create_session(data)
+
     response = RedirectResponse(url="/dashboard", status_code=303)
-    response.set_cookie(key='x-fastapicore-session', value='test')
+    response.set_cookie(key='x-fastapicore-session', value=session)
     
     return response
 
